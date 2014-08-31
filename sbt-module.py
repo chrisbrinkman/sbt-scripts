7ca0db7f31aab3ee2f701e7b82bed0703a02b30e
Chris Brinkman <chrisbrinkman@yahoo.com>
2014-08-31 11:50:29 -0400

    nice one

--- sbt-module.py
+++ sbt-module.py
@@ -1,31 +1,49 @@
 import os
-from sys import argv
 
-scalaVer = raw_input("scala version (2.11.2): ")
-name = raw_input("module name: ")
-ver = raw_input("module version (1.0-SNAPSHOT): ")
-rootDir = raw_input("root dir (.): ")
+def ensureDir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print("created dir {}".format(dir))
+    return;
 
-if scalaVer == "": scalaVer = "2.11.2"
-if ver == "": ver = "1.0-SNAPSHOT"
-if rootDir == "": rootDir  = "."
+def inputOrDefault(prompt, default):
+    val = input("{} ({}): ".format(prompt, default))
+    if val == "":
+        return default;
+    return val;
 
-if not os.path.exists(rootDir):
-    os.makedirs(rootDir)
+lines = []
+def appendSbtKeyVal(key, defaulVal, appendLineBreak: bool = True):
+    lines.append("\n{} := \"{}\"".format(key, inputOrDefault(key, defaulVal)))
+    if appendLineBreak:
+        lines.append("\n")
+    return;
 
-target = open(rootDir + "/build.sbt", 'w')
-target.write("name := \"" + name + "\"")
-target.write("\n")
-target.write("\nversion := \"" + ver + "\"")
-target.write("\n")
-target.write("\nscalaVersion := \"" + scalaVer + "\"")
-target.write("\n")
+# grab all values needed for the build.sbt file
+appendSbtKeyVal("name", "mymodule")
+appendSbtKeyVal("version", "1.0-SNAPSHOT")
+appendSbtKeyVal("scalaVersion", "2.11.2")
+
+# find the file and folder paths needed
+rootDir = inputOrDefault("root directory", ".")
+
+# ensure file locations
+ensureDir(rootDir)
+ensureDir("{}/src/main/scala".format(rootDir))
+ensureDir("{}/src/test/scala".format(rootDir))
+
+# write the build.sbt file
+buildFilePath = "{}/build.sbt".format(rootDir)
+target = open(buildFilePath, 'w')
+print("wrote...:")
+for l in lines:
+    target.write(l)
+    print(l, end="")
 target.close()
+print("\n...to file {}".format(buildFilePath))
 
-mainSrc = rootDir + "/src/main/scala"
-if not os.path.exists(mainSrc): os.makedirs(mainSrc)
 
-testSrc = rootDir + "/src/test/scala"
-if not os.path.exists(testSrc): os.makedirs(testSrc)
 
-print "wrote " + rootDir + "/build.sbt"
+
+
+
